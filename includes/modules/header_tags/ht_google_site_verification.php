74fd2ea73f2ad4765253b9d348d167bbe652b9eb
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2019-05-23 19:38:47 -0400

    reformat

--- includes/modules/header_tags/ht_google_site_verification.php
+++ includes/modules/header_tags/ht_google_site_verification.php
@@ -1,19 +1,20 @@
 <?php
-/**
- *
- *  @copyright 2008 - https://www.clicshopping.org
- *  @Brand : ClicShopping(Tm) at Inpi all right Reserved
- *  @Licence GPL 2 & MIT
- *  @licence MIT - Portion of osCommerce 2.4
- *  @Info : https://www.clicshopping.org/forum/trademark/
- *
- */
+  /**
+   *
+   * @copyright 2008 - https://www.clicshopping.org
+   * @Brand : ClicShopping(Tm) at Inpi all right Reserved
+   * @Licence GPL 2 & MIT
+   * @licence MIT - Portion of osCommerce 2.4
+   * @Info : https://www.clicshopping.org/forum/trademark/
+   *
+   */
 
   use ClicShopping\OM\Registry;
   use ClicShopping\OM\HTML;
   use ClicShopping\OM\CLICSHOPPING;
 
-  class ht_google_site_verification {
+  class ht_google_site_verification
+  {
     public $code;
     public $group;
     public $title;
@@ -21,20 +22,22 @@
     public $sort_order;
     public $enabled = false;
 
-    public function __construct() {
+    public function __construct()
+    {
       $this->code = get_class($this);
       $this->group = basename(__DIR__);
 
       $this->title = CLICSHOPPING::getDef('module_header_tags_google_site_verification_title');
       $this->description = CLICSHOPPING::getDef('module_header_tags_google_site_verification_description');
 
-      if ( defined('MODULE_HEADER_TAGS_GOOGLE_SITE_VERIFICATION_STATUS') ) {
+      if (defined('MODULE_HEADER_TAGS_GOOGLE_SITE_VERIFICATION_STATUS')) {
         $this->sort_order = MODULE_HEADER_TAGS_GOOGLE_SITE_VERIFICATION_SORT_ORDER;
         $this->enabled = (MODULE_HEADER_TAGS_GOOGLE_SITE_VERIFICATION_STATUS == 'True');
       }
     }
 
-    public function execute() {
+    public function execute()
+    {
       $CLICSHOPPING_Template = Registry::get('Template');
 
       if (!empty(MODULE_HEADER_TAGS_GOOGLE_SITE_VERIFICATION_CODE) && MODULE_HEADER_TAGS_GOOGLE_SITE_VERIFICATION_STATUS == 'True') {
@@ -42,15 +45,18 @@
       }
     }
 
-    public function isEnabled() {
+    public function isEnabled()
+    {
       return $this->enabled;
     }
 
-    public function check() {
+    public function check()
+    {
       return defined('MODULE_HEADER_TAGS_GOOGLE_SITE_VERIFICATION_STATUS');
     }
 
-    public function install() {
+    public function install()
+    {
       $CLICSHOPPING_Db = Registry::get('Db');
 
       $CLICSHOPPING_Db->save('configuration', [
@@ -88,20 +94,22 @@
           'date_added' => 'now()'
         ]
       );
-      
+
 
       return $CLICSHOPPING_Db->save('configuration', ['configuration_value' => '1'],
-                                               ['configuration_key' => 'WEBSITE_MODULE_INSTALLED']
-                            );
+        ['configuration_key' => 'WEBSITE_MODULE_INSTALLED']
+      );
     }
 
-    public function remove() {
+    public function remove()
+    {
       return Registry::get('Db')->exec('delete from :table_configuration where configuration_key in ("' . implode('", "', $this->keys()) . '")');
     }
 
-    public function keys() {
+    public function keys()
+    {
       return array('MODULE_HEADER_TAGS_GOOGLE_SITE_VERIFICATION_STATUS',
-                   'MODULE_HEADER_TAGS_GOOGLE_SITE_VERIFICATION_CODE',
-                   'MODULE_HEADER_TAGS_GOOGLE_SITE_VERIFICATION_SORT_ORDER');
+        'MODULE_HEADER_TAGS_GOOGLE_SITE_VERIFICATION_CODE',
+        'MODULE_HEADER_TAGS_GOOGLE_SITE_VERIFICATION_SORT_ORDER');
     }
   }
